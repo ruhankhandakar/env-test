57b4f170d51eb47c3fb9234948f04049535e40fd
Ruhan Khandakar <khandakarruhan@gmail.com>
2021-07-20 14:32:35 +0530

    check var

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {process.env.REACT_APP_TEST_ENV}
+        {process.env.VARIABLE}
         <a
           className="App-link"
           href="https://reactjs.org"
