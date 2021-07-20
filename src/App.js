b6705ec7c7c37e0f3c81482aab7b1e22eb7e50e0
Ruhan Khandakar <khandakarruhan@gmail.com>
2021-07-20 14:37:43 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {process.env.VARIABLE}
+        {process.env.REACT_APP_VARIABLE}
         <a
           className="App-link"
           href="https://reactjs.org"
